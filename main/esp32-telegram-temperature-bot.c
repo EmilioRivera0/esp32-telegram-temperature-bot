eaf3a31b347d9dad93a70dc767c35c82f8ea16af
EmilioRivera0 <elcholula619@gmail.com>
2025-03-12 23:16:50 -0600

    Feature: Main program while loop

--- main/esp32-telegram-temperature-bot.c
+++ main/esp32-telegram-temperature-bot.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include "esp_system.h"
 #include "dht11.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
     static struct dht11_reading stDht11Reading;
     DHT11_init(GPIO_NUM_25);
-    stDht11Reading = DHT11_read();
+
+    while (1){
+        stDht11Reading = DHT11_read();
     
-    if(DHT11_OK == stDht11Reading.status)
-    {
-        printf("Temperature: %d\n\n\n", stDht11Reading.temperature);
-    } 
-    else
-    {
-        printf("Error\n\n\n");
+        if(DHT11_OK == stDht11Reading.status)
+        {
+            printf("Temperature: %d\n\n\n", stDht11Reading.temperature);
+        } 
+        else
+        {
+            printf("Error\n\n\n");
+        }
+        //fflush(stdout);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    fflush(stdout);
-    esp_restart();
 }
